d8767a32e4970f1ecc8033fba59ad9ea0bc8f4b7
Emile ELRIC <elric.emile@gmail.com>
2025-01-22 14:59:27 +0100

    Lich

--- Assets/Script/NPC/Enemies/Lich.cs
+++ Assets/Script/NPC/Enemies/Lich.cs
@@ -4,17 +4,45 @@ using UnityEngine;
 
 public class Lich : Enemy
 {
+    public GameObject zombie;
+
     float invokeT = .0f;
     float invokeInterval = 2.0f;
+    float invokeDuration = 2.0f;
     int q = 3;
-
+    bool isSummoning;
      
+
+    void Summon()
+    {
+        GameObject z = Instantiate(zombie);
+        var c = Random.insideUnitCircle;
+        z.transform.position = transform.position + new Vector3(c.x, 0, c.y);
+        isSummoning = false;
+        invokeDuration = 2.0f;
+        invokeT = invokeInterval;
+    }
 
     protected override void Update()
     {
-        
-        base.Update();
 
-        invokeT -= Time.deltaTime;
+        if (isSummoning)
+        {
+            invokeDuration -= Time.deltaTime;
+            if (invokeDuration < 0)
+                Summon();
+        }
+        else
+        {
+            base.Update();
+            
+            invokeT -= Time.deltaTime;
+            if (invokeT < 0)
+            {
+                invokeT = invokeInterval;
+                isSummoning = true;
+            }
+        }
+        
     }
 }
