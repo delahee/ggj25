f601349bdc4e90d7149c71402f51d11965c9ef39
Emile ELRIC <elric.emile@gmail.com>
2025-01-22 08:30:02 +0100

    Heroes, ennemies & door

---
+++ Assets/Script/NPC/Door.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public int hp = 10;
+    bool dead = false;
+
+    public void OnHit()
+    {
+        if (dead) return;
+        hp--;
+        if (hp <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        dead = true;
+        Debug.Log("GameOver");
+        Application.Quit();
+    }
+}
--- Assets/Script/NPC/Enemy.cs
+++ Assets/Script/NPC/Enemy.cs
@@ -2,7 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Enemy : MonoBehaviour
 {
-        
+    public int speed = 3;
+
+    public Door door;
+    public int hp = 10;
+
+    public float atkMaxDuration = 2.0f;
+    public float atkT = 0f;
+
+    private void Start()
+    {
+        if (door == null)
+        {
+            FindDoor();
+        }
+        if (door == null)
+        {
+            enabled = false;
+            Debug.LogWarning("Door not found");
+        }
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, door.transform.position) > 1)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, door.transform.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            AttackSequence();
+        }
+    }
+
+    void FindDoor()
+    {
+        door = FindObjectOfType<Door>();
+    }
+
+    void AttackSequence()
+    {
+        atkT -= Time.deltaTime;
+        if (atkT < 0.0f)
+            Attack();
+    }
+
+    void Attack()
+    {
+        atkT = Random.Range(0f, atkMaxDuration);
+        door.OnHit();
+    }
+
+    public void OnHit()
+    {
+        hp--;
+        if (hp < 0)
+        {
+            OnDie();
+        }
+    }
+
+    void OnDie()
+    {
+        Destroy(gameObject);
+    }
+
 }
