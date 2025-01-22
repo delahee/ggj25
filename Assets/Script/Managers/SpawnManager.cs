29506600641c497a5e69cc90c9b7bfe5ea3a4001
Emile ELRIC <elric.emile@gmail.com>
2025-01-22 12:12:33 +0100

    EnemyData

--- Assets/Script/Managers/SpawnManager.cs
+++ Assets/Script/Managers/SpawnManager.cs
@@ -1,25 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+[Serializable]
+public struct EnemySpawn
+{
+    public Enemies e;
+    public float cooldown;
+    public int q;
+}
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject prefab;
+    public List<EnemySpawn> spawnList;
+    public float timer = 1.0f;
 
     private void Awake()
     {
-        StartCoroutine(SpawnRoutine()); 
+        StartCoroutine(SpawnRoutine());
     }
 
     IEnumerator SpawnRoutine()
     {
-        yield return new WaitForSeconds(.5f);
-        Spawn();
-        StartCoroutine(SpawnRoutine());
+        foreach (var spawn in spawnList)
+        {
+            yield return new WaitForSeconds(spawn.cooldown);
+            for (int i = 0; i < spawn.q; i++)
+            {
+                Enemy e = Instantiate(spawn.e.prefab).GetComponent<Enemy>();
+                if (e != null) e.Init(spawn.e);
+                SetPosition(e.gameObject);
+                yield return new WaitForSeconds(timer);
+            }
+        }
     }
 
-    void Spawn()
+    void SetPosition(GameObject go)
     {
-        GameObject go = Instantiate(prefab, transform.position, Quaternion.identity);
+        go.transform.position = transform.position;
     }
 }
