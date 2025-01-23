4317fbc6fafae1db798b74fa6d56bc8b60d6e208
Emile ELRIC <elric.emile@gmail.com>
2025-01-23 15:42:09 +0100

    Update EyeProj

--- Assets/Script/NPC/EyeProj.cs
+++ Assets/Script/NPC/EyeProj.cs
@@ -4,35 +4,37 @@ using UnityEngine;
 
 public class EyeProj : MonoBehaviour
 {
+    public Angel owner;
     public Vector3 targetPos;
     public float speed = 3.0f;
-    public float radius = 3.0f;
+    public float exploRadius = 3.0f;
     public int dmg;
     public bool dead;
     public bool hitEnemies;
+    [Space]
+    public SpriteRenderer explo;
 
     private void Start()
     {
+        explo.gameObject.SetActive(false);
         
+    }
+
+    private void OnValidate()
+    {
+        explo.transform.localScale = Vector3.one * exploRadius;
     }
 
     private void Update()
     {
-        if (dead) return;
-
+        if (dead)
+        {
+            explo.transform.localScale *= 1 - Time.deltaTime*10 ;
+            return;
+        }
         transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
         if (Vector3.Distance(transform.position, targetPos) < .1f)
         {
-            var hits = Physics.OverlapSphere(transform.position, radius);
-            foreach (var hit in hits) {
-                Hero h = hit.GetComponent<Hero>();
-                if (!hitEnemies) continue;
-                Enemy e = hit.GetComponent<Enemy>();
-                if (e != null)
-                {
-                    e.OnHit(dmg);
-                }
-            }
             Destroy();
         }
     }
@@ -40,12 +42,41 @@ public class EyeProj : MonoBehaviour
     void Destroy()
     {
         dead = true;
-        Destroy(gameObject);
+        explo.gameObject.SetActive(true);
+        var hits = Physics.OverlapSphere(transform.position, exploRadius);
+        foreach (var hit in hits)
+        {
+            Hero h = hit.GetComponent<Hero>();
+            if (!hitEnemies) continue;
+            Enemy e = hit.GetComponent<Enemy>();
+            if (e != null)
+            {
+                if (e != owner)
+                    e.OnHit(dmg);
+            }
+        }
+
+        Destroy(gameObject, .10f);
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, radius);   
+        Gizmos.DrawWireSphere(transform.position, exploRadius);   
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var h = other.GetComponent<Hero>();
+        var e = other.GetComponent<Enemy>();
+        if (h != null)
+        {
+            // APply dmg
+        }
+        if (e != null)
+        {
+            if (e != owner)
+                e.OnHit(1);
+        }
     }
 }
