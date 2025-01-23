26fe165404a1d69ee0cde257c69aec25099650d0
Emile ELRIC <elric.emile@gmail.com>
2025-01-23 12:17:41 +0100

    Angel class

---
+++ Assets/Script/NPC/Enemies/Angel.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Angel : Enemy
+{
+    [Space]
+    public EyeProj prefab;
+    public float ShootInterval = .8f;
+    public float t = .8f;
+
+    protected override void Update()
+    {
+        base.Update();
+        t -= Time.deltaTime;
+        if ( t <= 0)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        t = ShootInterval;
+
+        if (door != null)
+        {
+            EyeProj proj = Instantiate(prefab, transform.position, Quaternion.identity);
+            proj.targetPos = door.transform.position;
+            proj.dmg = dmg;
+        }
+        else
+            door = GateOfHell.instance;
+    }
+}
