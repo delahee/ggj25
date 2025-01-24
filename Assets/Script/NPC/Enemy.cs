43babe2fde60d9db6cb480256311e6b299969081
Marion Denmat <marion@headbang.club>
2025-01-24 11:20:50 +0100

    revomed som useless using

--- Assets/Script/NPC/Enemy.cs
+++ Assets/Script/NPC/Enemy.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Data.Common;
-using System.Xml.Linq;
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 
 
