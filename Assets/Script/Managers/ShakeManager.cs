cfb52c60bd30e9b4c3bbabe9e25b448a76bfeaa8
Aapef <jean01021999@gmail.com>
2025-02-21 16:02:28 +0100

    Ajout de juice sur les currency

--- Assets/Script/Managers/ShakeManager.cs
+++ Assets/Script/Managers/ShakeManager.cs
@@ -6,11 +6,33 @@ using static UnityEngine.GraphicsBuffer;
 
 public class ShakeManager : MonoBehaviour
 {
-    
-    public void OnClick(Transform Target)
+
+    private static ShakeManager instance;
+    public static ShakeManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                Debug.LogError("ShakeManager is null !!");
+
+            return instance;
+        }
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (instance != null)
+            Destroy(Instance.gameObject);
+
+        instance = this;
+    }
+
+    /*public void OnClick(Transform Target)
     {
         ObjectShaker(Target, new Vector3(1,0,0), 20, 3, 1.1f, false, false);
-    }
+    }*/
     public void ObjectShaker (Transform Target, Vector3 Direction, float Amplitude = 0f, int Frequency = 1, float Loss = 1f, bool Rotational = false, bool Fragmented = false)
     {
         StartCoroutine(ObjectShakerR(Target, Direction, Amplitude, Frequency, Loss, Rotational, Fragmented));
